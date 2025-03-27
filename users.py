ca541867ad8a0def181075d73eebdb63e29a11d5
Jyrinki Santtu V I <jysa@cubbli22-gold.local>
2025-03-27 12:01:46 +0200

    Refactor registration/login

--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+from werkzeug.security import check_password_hash, generate_password_hash
 import db
 
 def get_user(user_id):
@@ -8,3 +9,21 @@ def get_user(user_id):
 def get_items(user_id):
     sql = "SELECT id, title FROM items WHERE user_id = ? ORDER BY id DESC"
     return db.query(sql, [user_id])
+
+def create_user(username, password):
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])[0]
+    if not result:
+        return None
+
+    user_id = result["id"]
+    password_hash = result["password_hash"]
+    if check_password_hash(password_hash, password):
+        return user_id
+    else:
+        return None
